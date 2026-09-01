9d33d32be288004218ed825efb99dc6f1e6b6357
regefreak-cyber <regefreak@gmail.com>
2026-09-01 12:02:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -581,7 +581,7 @@ def tg_send_msg(chat_id: int, text: str):
 def tg_send_otp(otp: str, msg_text: str):
     kb = {
         "inline_keyboard": [
-            [{"text": f"{otp}", "copy_text": {"text": otp}}],
+            [{"text": f"📩 {otp}", "pay": True}],
             [{"text": "All Files", "url": "https://t.me/matchaappp"}],
         ]
     }
